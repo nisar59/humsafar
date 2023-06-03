a8517cd959f87ae5d4d57bebd8e8961bf9738558
Mahar Nisar <umair.awan@akhuwat.org.pk>
2023-06-03 11:55:28 +0500

    commit subscription export

--- Modules/ClientsSubscriptions/Resources/views/export.blade.php
+++ Modules/ClientsSubscriptions/Resources/views/export.blade.php
@@ -1,40 +1,103 @@
 <table>
     <thead>
     <tr>
-    <th>Client CNIC</th>
-     <th>Client Name</th>
-     <th>Client Email</th>
-     <th>Client Phone</th>
-      <th>Package</th>
-      <th>Amount</th>
-      <th>Subscription Date</th>
-      <th>Expire Date</th>
-      <th>Relation</th>
+    <th>First Name</th>
+     <th>Last Name</th>
+     <th>Email</th>
+     <th>Phone</th>
+     <th>Company Email</th>
+     <th>Employee ID</th>
+     <th>Marital Status</th>
+     <th>Date of Birth</th>
+     <th>CNIC</th>
+     <th>Gender</th>
+     <th>Country</th>
+     <th>State</th>
+     <th>City</th>
+     <th>Package</th>
+     <th>Amount</th>
+     <th>Subscription Date</th>
+     <th>Expire Date</th>
+     <th>Relation</th>
     </tr>
     </thead>
     <tbody>
         @foreach($subscriptions as $sub)
+
+            @php
+                $client=ClientDetail($sub->client_id);
+            @endphp
+
             <tr>
                 <td>
-                    @if(ClientDetail($sub->client_id)!=null)
-                       {{ClientDetail($sub->client_id)->cnic}}
+                    @if($client!=null)
+                       {{$client->name}}
                     @endif
                 </td>                
                 <td>
-                    @if(ClientDetail($sub->client_id)!=null)
-                       {{ClientDetail($sub->client_id)->name}}
+                    @if($client!=null)
+                       {{$client->parentage}}
                     @endif
+                </td> 
+                <td>
+                    @if($client!=null)
+                       {{$client->email}}
+                    @endif
+                </td> 
+                <td>
+                    @if($client!=null)
+                       {{$client->phone_primary}}
+                    @endif
+                </td> 
+                <td>
+                    @if($client!=null)
+                       {{$client->email}}
+                    @endif
+                </td> 
+                <td>
+                    @if($client!=null)
+                       {{$client->cnic}}
+                    @endif
+                </td> 
+                <td>
+                    @if($client!=null)
+                       @if($client->marital_status==1)
+                        Married
+                       @else
+                        Unmarried
+                       @endif
+                    @endif
+                </td> 
+                <td>
+                    @if($client!=null)
+                       {{Carbon\Carbon::parse($client->dob)->format('d-m-Y');}}
+                    @endif
+                </td> 
+                <td>
+                    @if($client!=null)
+                       {{$client->cnic}}
+                    @endif
+                </td> 
+                <td>
+                    @if($client!=null)
+                       {{$client->gender}}
+                    @endif
+                </td> 
+                <td>
+                    Pakistan
                 </td>
                 <td>
-                    @if(ClientDetail($sub->client_id)!=null)
-                       {{ClientDetail($sub->client_id)->email}}
-                    @endif
+                    @if($client!=null)
+                       {{$client->province}}
+                    @endif                
                 </td>
                 <td>
-                    @if(ClientDetail($sub->client_id)!=null)
-                       {{ClientDetail($sub->client_id)->phone_primary}}
-                    @endif
-                </td>                
+                    @if($client!=null)
+                       {{$client->district}}
+                    @endif                
+                </td>
+
+
                 <td>
                     @if(PackageDetail($sub->package_id)!=null)
                        {{PackageDetail($sub->package_id)->title}}
